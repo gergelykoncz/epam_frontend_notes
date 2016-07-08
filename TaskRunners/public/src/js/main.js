9def9fa17ad815fdd7a2ecc23160238a74f34c53
Elek, Marton <marton.elek@dpc.hu>
2016-07-08 09:54:38 +0200

    Added a link to details page

--- TaskRunners/public/src/js/main.js
+++ TaskRunners/public/src/js/main.js
@@ -1,17 +1,17 @@
 define(['jquery', "pokeService", "pubSub", "windowScroll"], function ($, pokeService) {
-	$(function () {
-		function fetchPokemon() {
-			pokeService.getPokemon(function (result) {
-				$('<tr>' +
-					'<td>' + result.id + '<img src="' + result.image + '"></td>' +
-					'<td class="name">' + result.name + '</td>' +
-					'<td>' + result.getTypes() + '</td>' +
-					'<td>' + result.getMoves() + '</td>').appendTo($('tbody'));
-			});
-		}
+    $(function () {
+        function fetchPokemon() {
+            pokeService.getPokemon(function (result) {
+                $('<tr>' +
+                    '<td>' + result.id + '<img src="' + result.image + '"></td>' +
+                    '<td class="name"><a href="details.html?id="' + result.id + ' >' + result.name + '</a></td>' +
+                    '<td>' + result.getTypes() + '</td>' +
+                    '<td>' + result.getMoves() + '</td></tr>').appendTo($('tbody'));
+            });
+        }
 
-		$.subscribe('Scrolling', fetchPokemon);
+        $.subscribe('Scrolling', fetchPokemon);
 
-		fetchPokemon();
-	});
+        fetchPokemon();
+    });
 });
