273cc6fa07fbd764865916092a3577d273b6fcfd
Elek, Marton <marton.elek@dpc.hu>
2016-07-08 10:06:13 +0200

    Fixing link text

--- TaskRunners/public/src/js/main.js
+++ TaskRunners/public/src/js/main.js
@@ -4,7 +4,7 @@ define(['jquery', "pokeService", "pubSub", "windowScroll"], function ($, pokeSer
             pokeService.getPokemon(function (result) {
                 $('<tr>' +
                     '<td>' + result.id + '<img src="' + result.image + '"></td>' +
-                    '<td class="name"><a href="details.html?id="' + result.id + ' >' + result.name + '</a></td>' +
+                    '<td class="name"><a href="details.html?id=' + result.id + '">' + result.name + '</a></td>' +
                     '<td>' + result.getTypes() + '</td>' +
                     '<td>' + result.getMoves() + '</td></tr>').appendTo($('tbody'));
             });
